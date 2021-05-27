d9652e3730602464bbb86764d04f7b9303308ff8
Julia Zielonka <80490273+jwz99@users.noreply.github.com>
2021-05-27 07:53:38 +0200

    Update Main.java

--- src/kalkulatorWalut/Main.java
+++ src/kalkulatorWalut/Main.java
@@ -16,24 +16,25 @@ public class Main extends Calculator{
 	public static void main(String[] args) {
 		
 		while(true) {
+			
 			//ask user for the euro value and chosen currency and get user input.  
 			Scanner scanner = new Scanner(System.in); 
 			Scanner scanner2 = new Scanner(System.in);
-		    System.out.println("Enter the value in euro(please, use dots instead of commas)");
-		    String euro_value = scanner.nextLine();  
-		    System.out.println("Enter the currency");
-		    String currency = scanner2.nextLine(); 
+		   	System.out.println("Enter the value in euro(please, use dots instead of commas)");
+		    	String euro_value = scanner.nextLine();  
+		    	System.out.println("Enter the currency");
+		    	String currency = scanner2.nextLine(); 
 		    
-		    try {
+		    	try {
 		    	
-		    	//convert the given input.
-		    	System.out.println(convert(Double.parseDouble(euro_value), currency));
-		    }
-		    //catch the exception if the number format is incorrect. 
-		    catch(Exception nfe) 
-		    {
-		    	System.out.println("Could not read value or currency correctly");
-		    }
+		    		//convert the given input.
+		    		System.out.println(convert(Double.parseDouble(euro_value), currency));
+		    	}
+		    	//catch the exception if the number format is incorrect. 
+		    	catch(Exception nfe) 
+		    	{
+		    		System.out.println("Could not read value or currency correctly");
+		    	}
 		}
 	}
 }
